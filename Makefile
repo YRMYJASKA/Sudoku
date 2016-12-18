5c3f87d4a332a666f4ccae3b1c356e0cf5bd5fd7
YRMYJASKA <hjelt.jyry@gmail.com>
2016-12-18 16:28:49 +0100

    Moved the code into a new src directory

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=g++
-SRC=main.cpp sudoku.cpp
+SRC=src/main.cpp src/sudoku.cpp
 FLAGS=-std=c++11
 OUTPUT=sudoku
 all:
---
+++ src/sudoku.h
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+//Cell class: One sudoku table contains 9 cells
+//Facts:
+//- 3x3
+//- contains numbers through 1-9
+//- Sum of all numbers in it is 45
+class Cell{
+    public:
+        //Generation
+        Cell();
+        void generate();
+        //Grid
+        void setGrid(int params[3][3]);
+        void printGrid();
+        //Tools
+        bool hasNum(int y);
+        void printRow(int num);
+        void SetZero();
+        bool checkList(int n);
+        void removeFromList();
+        //The grid 
+        int grid[3][3];
+    private:
+        int whitelist[9] = {1,2,3,4,5,6,7,8,9};
+};
+class SudokuTable{
+    public:
+        SudokuTable();
+        void printTable();
+        void generateWhole();
+    private:
+       int tableGrid[3][3][3][3]; 
+};
+
+
